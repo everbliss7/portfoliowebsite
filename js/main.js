da4d913e50f29d490e6f094f992c5e029defc068
Blessing Tayedzerwa <everbliss7@gmail.com>
2019-06-27 06:01:57 +0200

    added a Google Search Console API, fix a touch bug, fixed a midea query style for the footer and h1.lg-heading for small screens

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // Select DOM Items
+'strict';
+
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
@@ -7,7 +9,30 @@ const navItems = document.querySelectorAll('.nav-item');
 
 // Set Initial State Of Menu
 let showMenu = false;
+let touch = function is_touch_device() {
+  var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+  var mq = function(query) {
+    return window.matchMedia(query).matches;
+  };
 
+  if (
+    'ontouchstart' in window ||
+    (window.DocumentTouch && document instanceof DocumentTouch)
+  ) {
+    return true;
+  }
+
+  // include the 'heartz' as a way to have a non matching MQ to help terminate the join
+  // https://git.io/vznFH
+  var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+  return mq(query);
+};
+
+if (touch) {
+  menuBtn.addEventListener('touchstart', toggleMenu);
+} else {
+  menuBtn.addEventListener('click', toggleMenu);
+}
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
